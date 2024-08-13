a163f0602f323e61d3146e5ee0032e178d1b9cc1
rizalahmadsyarif <rizalahmadsyariff@gmail.com>
2024-08-13 18:51:17 +0700

    this materi

---
+++ this.js
@@ -0,0 +1,30 @@
+// membuat Object Angkot
+function Angkot(sopir, trayek, penumpang, kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    this.penumpangNaik = function (namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    TimeRanges.penumpangTurun = function(namaPenumpang, bayar) {
+        if (this.penumpang.length == 0) {
+            alert('angkot masih kosong');
+            return false;
+        }
+        for (var i = 0; i < this.penumpang.length; i++) {
+            if( this.penumpang[i] == namaPenumpang) {
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
+    }
+}
+
+var angkot1 = new Angkot('Galih', ['Chicahem','Cibiru'], [], 0);
+var angkot2 = new Angkot('Tom', ['antapani', 'ciroyom'], [], 0);
+
